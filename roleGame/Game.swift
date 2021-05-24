6d93b975f5a94a486d7241604dfa33f767350f2e
Melissa Briere <briere.melissa@gmail.com>
2021-05-24 15:51:56 +0200

    add Game.swift

---
+++ roleGame/Game.swift
@@ -0,0 +1,26 @@
+//
+//  Game.swift
+//  roleGame
+//
+//  Created by Melissa Briere on 24/05/2021.
+//
+
+import Foundation
+
+class Game {
+    
+    func start() {
+        
+    }
+    
+    func createTeams() {
+        
+    }
+    
+    func startBatlle() {
+        
+    }
+    func displayWinner() {
+        
+    }
+}
